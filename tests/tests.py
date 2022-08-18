14a8dc36613451614506b790b0db382e554e6100
uzair local <uzair.m6d@gmail.com>
2022-08-18 12:00:42 -0400

    remove mock env variable

--- tests/tests.py
+++ tests/tests.py
@@ -1,11 +1,8 @@
 import unittest
-from unittest import mock
 import script.hi
 import numpy as np
 import os
 
-
-# @mock.patch.dict(os.environ, {"NAME": "Sial"})
 class Test(unittest.TestCase):
     def test1(self):
 
