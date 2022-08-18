1cde873fe3a64f3c952e750aa8f562769bdde6e4
uzair local <uzair.m6d@gmail.com>
2022-08-18 10:55:26 -0400

    add unit test

--- tests/tests.py
+++ tests/tests.py
@@ -1,8 +1,13 @@
 import unittest
+from unittest import mock
 import script.hi
+import numpy as np
+import os
 
 
+# @mock.patch.dict(os.environ, {"NAME": "Sial"})
 class Test(unittest.TestCase):
     def test1(self):
 
-        script.hi.say_hi()
+        x = script.hi.say_hi(os.environ.get('NAME'))
+        self.assertEqual(x, np.array([1]))
